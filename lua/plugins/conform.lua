c53e29318d5e8aae6894387df3f4c5c4d9f3bce3
ammarlokh95 <ammar.lokh1234@gmail.com>
2024-05-16 23:15:32 -0400

    remove ale and use conform for formatting

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,40 @@
+return {
+  "stevearc/conform.nvim",
+  dependencies = { "mason.nvim" },
+  lazy = true,
+  cmd = "ConformInfo",
+  keys = {
+    {
+      "<leader>cF",
+      function()
+        require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+      end,
+      mode = { "n", "v" },
+      desc = "Format Injected Langs",
+    },
+  },
+  opts = {
+    -- LazyVim will use these options when formatting with the conform.nvim formatter
+    format = {
+      timeout_ms = 3000,
+      async = false, -- not recommended to change
+      quiet = false, -- not recommended to change
+      lsp_fallback = true, -- not recommended to change
+    },
+    ---@type table<string, conform.FormatterUnit[]>
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "black" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      scss = { "prettier" },
+      css = { "prettier" },
+    },
+    -- The options you set here will be merged with the builtin formatters.
+    -- You can also define any custom formatters here.
+    ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+    formatters = {
+      injected = { options = { ignore_errors = true } },
+    },
+  },
+}
