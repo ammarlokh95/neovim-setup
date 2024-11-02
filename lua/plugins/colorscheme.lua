0d699ceb39e3a77924729f8cc7ad1551f492de03
ammarlokh95 <ammar.lokh1234@gmail.com>
2024-11-02 00:52:50 +0000

    change theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,24 +1,34 @@
+-- return {
+--   "catppuccin/nvim",
+--   name = "catppuccin",
+--   priority = 1000,
+--   config = function()
+--     require("catppuccin").setup({
+--       flavour = "mocha",
+--       transparent_background = true,
+--       custom_highlights = function(colors)
+--         local u = require("catppuccin.utils.colors")
+--         return {
+--           LineNr = {
+--             fg = colors.lavender,
+--           },
+--           CursorLineNr = {
+--             fg = colors.red,
+--             bg = u.lighten(colors.blue, 0.8),
+--           },
+--         }
+--       end,
+--     })
+--     vim.cmd("colorscheme catppuccin") end,
+-- }
+--
+
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  "rebelot/kanagawa.nvim",
+  name = "kanagawa",
   priority = 1000,
   config = function()
-    require("catppuccin").setup({
-      flavour = "mocha",
-      transparent_background = true,
-      custom_highlights = function(colors)
-        local u = require("catppuccin.utils.colors")
-        return {
-          LineNr = {
-            fg = colors.lavender,
-          },
-          CursorLineNr = {
-            fg = colors.red,
-            bg = u.lighten(colors.blue, 0.8),
-          },
-        }
-      end,
-    })
-    vim.cmd("colorscheme catppuccin")
+    require("kanagawa").setup({})
+    vim.cmd("colorscheme kanagawa")
   end,
 }
