77e2bf58f790f096d48a7c7d920e0f680093078c
Ammar Lokhandwala <alokhandwala@yprime.com>
2025-02-14 12:59:30 -0500

    Lazyload harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,14 +4,30 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
   },
-  config = function()
+  keys = {
+    {
+      "<leader>ha",
+      ":lua require('harpoon.mark').add_file()<CR>",
+      desc = "Add file to harpoon",
+    },
+    {
+      "<leader>hu",
+      ":Telescope harpoon marks<CR>",
+      desc = "Toggle harpoon telscope",
+    },
+    {
+      "<leader>hn",
+      ":lua require('harpoon.ui').nav_next()<CR>",
+      desc = "Harpoon nav next",
+    },
+    {
+      "<leader>hN",
+      ":lua require('harpoon.ui').nav_prev()<CR>",
+      desc = "Harpoon nav prev",
+    },
+  },
+  opts = function()
     require("harpoon").setup({})
     require("telescope").load_extension("harpoon")
-
-    local keymap = vim.keymap
-    keymap.set("n", "<leader>ha", ":lua require('harpoon.mark').add_file()<CR>", { desc = "Add file to harpoon" })
-    keymap.set("n", "<leader>hu", ":Telescope harpoon marks<CR>", { desc = "Toggle harpoon telscope" })
-    keymap.set("n", "<leader>hn", ":lua require('harpoon.ui').nav_next()<CR>", { desc = "Harpoon nav next" })
-    keymap.set("n", "<leader>hN", ":lua require('harpoon.ui').nav_prev()<CR>", { desc = "Harpoon nav prev" })
   end,
 }
