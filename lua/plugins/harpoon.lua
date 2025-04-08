99f385f0c72953e2a2aa332a3874ed457afbc91f
Ammar Lokhandwala <alokhandwala@yprime.com>
2025-04-08 12:14:19 -0400

    harpoon sensible shortcuts
    Remove Terminal specific navigation stuff

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,24 +6,49 @@ return {
   },
   keys = {
     {
-      "<leader>ha",
+      "ha",
       ":lua require('harpoon.mark').add_file()<CR>",
       desc = "Add file to harpoon",
     },
     {
-      "<leader>hu",
-      ":Telescope harpoon marks<CR>",
-      desc = "Toggle harpoon telscope",
+      "hu",
+      ":lua require('harpoon.ui').toggle_quick_menu()<CR>",
+      desc = "Toggle harpoon quick menu",
     },
     {
-      "<leader>hn",
+      "hn",
       ":lua require('harpoon.ui').nav_next()<CR>",
       desc = "Harpoon nav next",
     },
     {
-      "<leader>hN",
+      "hp",
       ":lua require('harpoon.ui').nav_prev()<CR>",
       desc = "Harpoon nav prev",
+    },
+    {
+      "h1",
+      ":lua require('harpoon.ui').nav_file(1)<CR>",
+      desc = "Harpoon nav file 1",
+    },
+    {
+      "h2",
+      ":lua require('harpoon.ui').nav_file(2)<CR>",
+      desc = "Harpoon nav file 2",
+    },
+    {
+      "h3",
+      ":lua require('harpoon.ui').nav_file(3)<CR>",
+      desc = "Harpoon nav file 3",
+    },
+    {
+      "h4",
+      ":lua require('harpoon.ui').nav_file(4)<CR>",
+      desc = "Harpoon nav file 4",
+    },
+    {
+      "h5",
+      ":lua require('harpoon.ui').nav_file(5)<CR>",
+      desc = "Harpoon nav file 5",
     },
   },
   opts = function()
