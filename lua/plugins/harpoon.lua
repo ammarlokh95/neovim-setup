9002d2bc994e92c7044a58faa2f36afd54d3b3e1
Ammar Lokhandwala <alokhandwala@yprime.com>
2025-04-14 10:13:25 -0400

    rechange harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,47 +6,47 @@ return {
   },
   keys = {
     {
-      "ha",
+      "<leader>ha",
       ":lua require('harpoon.mark').add_file()<CR>",
       desc = "Add file to harpoon",
     },
     {
-      "hu",
+      "<leader>hu",
       ":lua require('harpoon.ui').toggle_quick_menu()<CR>",
       desc = "Toggle harpoon quick menu",
     },
     {
-      "hn",
+      "<leader>hn",
       ":lua require('harpoon.ui').nav_next()<CR>",
       desc = "Harpoon nav next",
     },
     {
-      "hp",
+      "<leader>hp",
       ":lua require('harpoon.ui').nav_prev()<CR>",
       desc = "Harpoon nav prev",
     },
     {
-      "h1",
+      "<leader>h1",
       ":lua require('harpoon.ui').nav_file(1)<CR>",
       desc = "Harpoon nav file 1",
     },
     {
-      "h2",
+      "<leader>h2",
       ":lua require('harpoon.ui').nav_file(2)<CR>",
       desc = "Harpoon nav file 2",
     },
     {
-      "h3",
+      "<leader>h3",
       ":lua require('harpoon.ui').nav_file(3)<CR>",
       desc = "Harpoon nav file 3",
     },
     {
-      "h4",
+      "<leader>h4",
       ":lua require('harpoon.ui').nav_file(4)<CR>",
       desc = "Harpoon nav file 4",
     },
     {
-      "h5",
+      "<leader>h5",
       ":lua require('harpoon.ui').nav_file(5)<CR>",
       desc = "Harpoon nav file 5",
     },
